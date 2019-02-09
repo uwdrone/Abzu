b5de41cc4c18b6a157c87a57ce6a77c999676647
StefanVidakovic <vidakovi@ualberta.ca>
2019-02-09 15:00:00 -0700

    Changed host file to appropriate IP

--- CameraControl/templateFiles/SendData/testDataRecieve.py
+++ CameraControl/templateFiles/SendData/testDataRecieve.py
@@ -1,6 +1,8 @@
 import socket
 
-HOST = '192.168.1.100' # Server IP or Hostname
+HOST = '192.168.1.101' # Server IP or Hostname
+host = socket.gethostname()
+hostName = socket.gethostbyname('0.0.0.0')
 PORT = 12345 # Pick an open Port (1000+ recommended), must match the client sport
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 print 'Socket created'
@@ -8,13 +10,13 @@ print 'Socket created'
 #managing error exception
 try:
     s.bind((HOST, PORT))
-    except socket.error:
+except socket.error:
     print 'Bind failed '
-
-    s.listen(5)
-    print 'Socket awaiting messages'
-    (conn, addr) = s.accept()
-    print 'Connected'
+s.listen(5)
+print s.getsockname()
+print 'Socket awaiting messages'
+(conn, addr) = s.accept()
+print 'Connected'
 
 # awaiting for message
 while True:
@@ -25,7 +27,7 @@ while True:
     # process your message
     if data == 'Hello':
         reply = 'Hi, back!'
-        elif data == 'This is important':
+    elif data == 'This is important':
         reply = 'OK, I have done the important thing you have asked me!'
 
     #and so on and on until...
