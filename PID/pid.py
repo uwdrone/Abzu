4e6e4fce1a44c787ae97f160c281510006c636b1
bpomerleau <bpomerle@ualberta.ca>
2019-03-18 17:14:40 -0600

    begun PID

---
+++ PID/pid.py
@@ -0,0 +1,23 @@
+from threading import Thread
+import socket
+Class PID(Thread):
+    def __init__(self, inputMonitor, imuLock=None, imuData=None):
+        Thread.__init__(self)
+        self.inputMap = inputMonitor["inputMap"]
+        self.readLock = inputMonitor["readLock"]
+        self.writeLock = inputMonitor["writeLock"]
+        self.inputMonitor = inputMonitor
+
+        self.imuLock = imuLock
+        self.imuData = imuData
+
+    def
+
+### procedure ###
+# 1. get angle from IMU
+# 2. get controller values
+# 3. compute reference angle
+# 4. compute D term directly from IMU data
+# 5. compute error and transform to motor basis
+# 6. compute P and I
+# 7. sum P, I, and D; normalise and saturate
