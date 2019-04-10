468b6fdf1097c58c38a0e7230b7fb1abbb0c80b5
bpomerleau <bpomerleau@ualberta.ca>
2019-04-10 11:08:40 -0600

    scp videos script

---
+++ mv_vids.sh
@@ -0,0 +1,7 @@
+scp pi@192.168.1.102:/home/pi/Desktop/VideoRecordings/video* /home/brady/Videos/AbzuCam/
+status=$?
+if [[ $status -eq 0 ]]
+then
+	ssh pi@192.168.1.102 'rm /home/pi/Desktop/VideoRecordings/video*'
+fi
+ssh pi@192.168.1.102 'sudo shutdown now'
